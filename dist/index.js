d7e8a1dfd2d53bd41ab4233bdebce3fd7af0fcec
tkshnwesper <unknown7bolt@gmail.com>
2016-07-02 20:07:27 +0530

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,49 @@
+var horizon = Horizon();
+var messages = null;
+
+horizon.onReady(function() {
+    $(".mform").on("submit", function(e) {
+        e.preventDefault();
+    });
+
+    messages = horizon("messages");
+
+    var mlist = new Vue({
+        el: ".messages",
+        data: {
+            messageList: null
+        }
+    });
+
+    messages.order("datetime", "descending").limit(50).watch().subscribe((docs) => {
+        mlist.messageList = docs;
+    });
+
+    mlist.$watch('messageList', function(n, o) {
+        var m = $(".messages");
+        m.scrollTop(m[0].scrollHeight);
+    });
+});
+
+horizon.connect();
+
+var Message = function(text) {
+    this.datetime = new Date();
+    this.text = text;
+
+    this.toObject = function() {
+        return {
+            datetime: this.datetime,
+            text: this.text
+        }
+    }
+}
+
+function sendMessage() {
+    var msg = $(".mfield").val();
+    $(".mfield").val("");
+    if(msg && messages) {
+        var message = new Message(msg);
+        messages.store(message.toObject());
+    }
+}
